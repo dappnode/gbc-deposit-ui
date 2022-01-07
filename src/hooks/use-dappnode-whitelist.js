edb38ac6e4836e35e145ee4b6a8257422813ebda
pablo <mendez4a@gmail.com>
2022-01-07 17:02:12 +0100

    added logs for debugging

--- src/hooks/use-dappnode-whitelist.js
+++ src/hooks/use-dappnode-whitelist.js
@@ -6,12 +6,15 @@ function useDappnodeWhitelist (address, contract) {
 
   useEffect(() => {
     const getDappnodeWhitelist = async (contract) => {
-    // Check requirements: address is whitelisted and must not be expired
-    // addressToIncentive: https://blockscout.com/xdai/mainnet/address/0x6C68322cf55f5f025F2aebd93a28761182d077c3/contracts
+      // Check requirements: address is whitelisted and must not be expired
+      // addressToIncentive: https://blockscout.com/xdai/mainnet/address/0x6C68322cf55f5f025F2aebd93a28761182d077c3/contracts
       const addressToIncentive = await contract.addressToIncentive(address) // returns struct {endTime, isClaimed}
+      console.log(addressToIncentive)
 
       const isClaimed = addressToIncentive.isClaimed
       const endTime = parseInt(addressToIncentive.endTime)
+      console.log("isClaimed:", isClaimed)
+      console.log("endTime: ", endTime)
 
       if (isClaimed) return { isWhitelisted: false, message: 'Address has already been claimed' }
       if (endTime !== 0) return { isWhitelisted: false, message: 'Address is not whitelisted' }
